500745c7f821fb19d16de1e4f56ea2cd771fbe4b
emanuelhardwell <emanuelnoe16@gmail.com>
2020-12-04 01:19:43 -0600

    Model for images created

---
+++ src/models/image.js
@@ -0,0 +1,23 @@
+/*  */
+/*  */
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const path = require("path");
+
+
+const ImageSchema = new Schema({
+  title: { type: String },
+  description: { type: String },
+  filename: { type: String },
+  views: { type: Number, default: 0 },
+  likes: { type: Number, default: 0 },
+  date: { type: Date, default: Date.now },
+});
+
+/* esquema virtual nos servira para ocultar la extension de las imagenes
+ y mostrar solo el nombre en la URL */
+ImageSchema.virtual("uniqueId").get(function () {
+  return this.filename.replace(path.extname(this.filename), "");
+});
+
+module.exports = mongoose.model("Image", ImageSchema);
---
+++ src/models/indexModels.js
@@ -0,0 +1,5 @@
+/*  */
+/*  */
+module.exports = {
+  Image: require("./image"),
+};
